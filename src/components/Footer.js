515b1524fe896cfc86012baa6d0b401ce18ace3b
Shinn Thant Swam Ye <1shinnthantswamye@gmail.com>
2023-10-20 01:51:57 +0630

    Add footer and styles

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import '../Styles/Footer.css';
+
+const Footer = () => (
+  <footer className= "footer">
+    <p className= "author">
+      ©️ Shinn Thant Swam Ye
+      {' '}
+      <a href="https://github.com/Pi1998" target="_blank" className= "link" rel="noreferrer">@Pi1998</a>
+    </p>
+    <p className= "designCredit">
+      MarvelFolio design inspired by
+      {' '}
+      <a href="https://www.behance.net/sakwadesignstudio" target="_blank" className= "link" rel="noreferrer">Nelson Sakwa</a>
+    </p>
+  </footer>
+);
+
+export default Footer;
