1fd13eb591e3e888f23c3dcce159bda54b9a46a0
Shinn Thant Swam Ye <1shinnthantswamye@gmail.com>
2023-10-20 14:44:20 +0630

    Modify search bar, update individual banner

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,33 +1,45 @@
 import { useParams } from 'react-router';
-import { useSelector } from 'react-redux';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { updateCurrentPage } from '../Redux/Character/charactersSlice';
 import '../Styles/Details.css';
 
 const Details = () => {
-    const dispatch = useDispatch();
-  
-    useEffect(() => {
-      dispatch(updateCurrentPage('Details'));
-    }, [dispatch]);
-
+  const dispatch = useDispatch();
   const { id } = useParams();
   const characters = useSelector((state) => state.characters.characters);
   const character = characters.find((char) => char.id === parseInt(id, 10));
 
+  useEffect(() => {
+    dispatch(updateCurrentPage('Details'));
+  }, [dispatch, id]);
+
   return (
-    <div className= "detailsContainer">
-      <div className= "banner" />
-      <div className= "searchBar">
-        <p className="info">Character Details</p>
+    <div className="detailsContainer">
+      {character && (
+        <div
+          className="banner"
+          style={{
+            background: `url(${character.thumbnail.path}.${character.thumbnail.extension})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+      )}
+      <div className="searchBar">
+        <p className="info">[{character.name}] Available Comics '{character.comics.available}'</p>
       </div>
-      <div className= "charactersContainer">
+      <div className="charactersContainer">
         {character && (
           <div className="characters" key={character.id}>
-            <img className= "img" src={`${character.thumbnail.path}.${character.thumbnail.extension}`} alt={character.name} />
-            <p className= "name">{character.name}</p>
-            <p className= "title">Comics available: {character.comics.available}</p>
+            {character.comics.items.map((comic, index) => (
+              <div
+                key={comic.resourceURI}
+                className={index % 2 === 0 ? 'even' : 'odd'}
+              >
+                {comic.name}
+              </div>
+            ))}
           </div>
         )}
       </div>
