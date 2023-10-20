390a5c8090bfa0081efb7850354a93412c8128c3
Shinn Thant Swam Ye <1shinnthantswamye@gmail.com>
2023-10-20 19:13:44 +0630

    Fix axios error

--- src/Redux/Character/charactersSlice.js
+++ src/Redux/Character/charactersSlice.js
@@ -1,21 +1,25 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
 import md5 from 'md5';
 
 const publicKey = '8d8b758839fc1789f13d52ae6a71b1e6';
 const privateKey = '57798680b7f101a6b04009d15f5266c93f4fab67';
 
 const timestamp = new Date().getTime().toString();
-const hash = md5(timestamp + privateKey + publicKey); 
+const hash = md5(timestamp + privateKey + publicKey);
 
 export const getData = createAsyncThunk('characters/getMarvelCharacters', async () => {
   const apiUrl = `https://gateway.marvel.com/v1/public/characters?apikey=${publicKey}&ts=${timestamp}&hash=${hash}`;
 
   try {
-    const response = await axios.get(apiUrl);
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
 
-    if (response.data && response.data.data && response.data.data.results) {
-      const filteredData = response.data.data.results.map((character) => ({
+    const data = await response.json();
+
+    if (data && data.data && data.data.results) {
+      const filteredData = data.data.results.map((character) => ({
         id: character.id,
         name: character.name,
         comics: {
@@ -57,7 +61,7 @@ const characterSlice = createSlice({
       state.currentPage = payload;
     },
     setCharacterName: (state, { payload }) => {
-        state.characterName = payload;
+      state.characterName = payload;
     },
   },
   extraReducers: (builder) => {
